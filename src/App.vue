e157910d69eb089a659d3628a263051f11213a1b
Maksim <m.shumkov@einmahl-works.de>
2025-12-16 15:41:50 +0100

    kommische Todo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
-<template><h1>On Deploy</h1></template>
+const todo = ref("");
+
+function addTodo() {}
+</script>
+
+<template>
+  <h1>On Deploy</h1>
+  <input v-model="todo" type="text" placeholder="Tippen Sie Todo" />
+  <button @click="addTodo()">Add</button>
+
+  <p>{{ todo }}</p>
+</template>
 
 <style scoped></style>
